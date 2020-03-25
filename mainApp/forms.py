5996a18000aae95820832998fa0862068408c41a
HashirGJ8842 <hameedhashirniazi@gmail.com>
2020-03-25 13:56:53 +0530

    Added type

--- mainApp/forms.py
+++ mainApp/forms.py
@@ -9,6 +9,21 @@ class RegistrationForm(FlaskForm):
     teamSize = SelectField('Team Size', validators=[DataRequired()], choices=[('0', 'Please select one'), ('1', '1'), ('2', '2'), ('3', '3')])
     teamMember_1 = StringField('Member 1', validators=[Length(min=2, max=20)])
     teamMember_2 = StringField('Member 2', validators=[Length(min=2, max=20)])
-    themes = SelectField('Theme', validators=[DataRequired()], choices=[('None', 'Select'), ('smart', 'Smart City/ Swachh Bharat'), ('sustain', 'Sustainable Development'), ('health', 'HealthCare and Biomedical Services'), ('security', "Security and Surveillance"), ('innovate', 'Student Innovation'), ('COVID', 'COVID-19 and Quarantine')])
+    themesS = SelectField('Theme', validators=[DataRequired()], choices=[('None', 'Select'), ('smart', 'Smart City/ Swachh Bharat'), ('sustain', 'Sustainable Development'), ('health', 'HealthCare and Biomedical Services'), ('security', "Security and Surveillance"), ('innovate', 'Student Innovation'), ('COVID', 'COVID-19 and Quarantine')])
+    themesE = SelectField('Theme', validators=[DataRequired()],
+                          choices=[('None', 'Select'), ('smart', 'Smart City/ Swachh Bharat'),
+                                   ('sustain', 'Sustainable Development'),
+                                   ('health', 'HealthCare and Biomedical Services'),
+                                   ('security', "Security and Surveillance"), ('innovate', 'Student Innovation'),
+                                   ('COVID', 'COVID-19 and Quarantine')])
+    themesM = SelectField('Theme', validators=[DataRequired()],
+                          choices=[('None', 'Select'), ('arm', 'Robotic Arm'),
+                                   ('solar', 'Origami Based Solar Panels'),
+                                   ('wheel', 'Advanced Wheel Steering Mechanism'),
+                                   ('e_vehicle', "Driverless E-Vehicle Design"), ('mask', 'COVID-19 Mask')])
+    type = SelectField('Type', validators=[DataRequired()],
+                         choices=[('None', 'Select'), ('software', 'Software'),
+                                  ('electronics', 'Electronics'),
+                                  ('mechanical', 'Mechanical Design')])
     submit = SubmitField('Signup')
     recaptcha = RecaptchaField()
